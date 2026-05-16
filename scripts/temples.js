3d304186360599a9dd8f90caee11a4dc7a46ecbe
clementjnr <paulclement711@gmail.com>
2026-05-16 08:13:40 +0100

    update all changes

---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("hide");
+});
+
+document.querySelector("#currentyear").textContent =
+    new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
